af2ef507bb2d6f924f6803ce29a740b5d8679415
somda <94lfnv@gmail.com>
2023-10-31 14:54:35 +0900

    practice for full-stack

---
+++ client/src/components/App.js
@@ -0,0 +1,40 @@
+import React, { Suspense } from "react";
+import { Route, Switch } from "react-router-dom";
+import Auth from "../hoc/auth";
+// pages for this product
+import LandingPage from "./views/LandingPage/LandingPage.js";
+import LoginPage from "./views/LoginPage/LoginPage.js";
+import RegisterPage from "./views/RegisterPage/RegisterPage.js";
+import NavBar from "./views/NavBar/NavBar";
+import Footer from "./views/Footer/Footer";
+import VideoUploadPage from "./views/videoupload/VideoUploadPage";
+
+//null   Anyone Can go inside
+//true   only logged in user can go inside
+//false  logged in user can't go inside
+
+function App() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <NavBar />
+      <div style={{ paddingTop: "69px", minHeight: "calc(100vh - 80px)" }}>
+        <Switch>
+          <Route exact path="/" component={Auth(LandingPage, null)} />
+          {/* null 이면 아무나 들어갈 수 있음 false는 로그인한 유저는 들어갈 수 없음
+          이미 로그인을 했으니 로그인 페이지나 등록 페이지에 들어갈 필요가 없다
+          true면 로그인한 유저만 들어갈 수 있음 */}
+          <Route exact path="/login" component={Auth(LoginPage, false)} />
+          <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route
+            exact
+            path="/video/upload"
+            component={Auth(VideoUploadPage, true)}
+          />
+        </Switch>
+      </div>
+      <Footer />
+    </Suspense>
+  );
+}
+
+export default App;
